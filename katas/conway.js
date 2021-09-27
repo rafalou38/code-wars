885082b6711d776ee10b9d00799f6a93779ab5dc
rafalou38 <58974586+rafalou38@users.noreply.github.com>
2021-09-27 11:32:10 +0200

    conway working for one generation

--- katas/conway.js
+++ katas/conway.js
@@ -10,8 +10,9 @@ function getGeneration(rows, generations) {
     let new_gen = []
 
 
-    // add one cell around all
-    rows = rows.map(row=>{
+    
+    // add one cell of padding around the grid
+    rows = rows.map(row => {
         return [
             0,
             ...row,
@@ -19,50 +20,42 @@ function getGeneration(rows, generations) {
         ]
     })
     rows = [
-        new Array(width+2).fill(0),
+        new Array(width + 2).fill(0),
         ...rows,
-        new Array(width+2).fill(0),
+        new Array(width + 2).fill(0),
     ]
 
-    // fill new gen
+    // reset new gen
     new_gen = new Array(width).fill().map(
-        e=>new Array(height).fill(0)
+        e => new Array(height).fill(0)
     )
-    console.log({rows,new_gen})
 
-    //console.log(htmlize(rows));
-    for (let y = 1; y < rows.length-1; y++) {
+    for (let y = 1; y < rows.length - 1; y++) {
         const row = rows[y];
-        for (let x = 1; x < row.length-1; x++) {
+        for (let x = 1; x < row.length - 1; x++) {
             const cell = row[x];
             const neigbours = 0
                 + rows[y - 1][x - 1]
                 + rows[y - 1][x]
                 + rows[y - 1][x + 1]
                 + rows[y][x - 1]
-                //+ rows[y][x]
                 + rows[y][x + 1]
                 + rows[y + 1][x - 1]
                 + rows[y + 1][x]
                 + rows[y + 1][x + 1]
-            
-            if (neigbours < 2 || neigbours > 3) {
-                new_gen[y-1][x-1] = 0
-            }else{
-                new_gen[y-1][x-1] = 1
-            }
 
-            console.log({
-                y,x,neigbours
-            });
+            if (cell) {
+                if (neigbours < 2) {
+                    new_gen[y - 1][x - 1] = 0
+                } else if (neigbours > 3) {
+                    new_gen[y - 1][x - 1] = 0
+                }else{
+                    new_gen[y - 1][x - 1] = 1
+                }
+            } else if (neigbours == 3) {
+                new_gen[y - 1][x - 1] = 1
+            }
         }
     }
     return new_gen
 }
-
-
-getGeneration([
-    [1, 0, 0],
-    [0, 1, 1],
-    [1, 1, 0]
-  ])
